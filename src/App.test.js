bbb265fafdabdf7c1748104d6db6e93d6419420b
Camilo Utima <juancamiloutima@gmail.com>
2022-11-28 18:58:09 -0500

    Agregados test a fetch y al buscador

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,8 @@ import App from "./App";
 
 test("renders the landing page", () => {
   render(<App />);
-  //test implementados en clase de Prospercity
   expect(screen.getByRole("heading")).toHaveTextContent(/Doggy Directory/);
   expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a breed");
   expect(screen.getByRole("button", { name: "Search" })).toBeDisabled();
-  expect(screen.getByRole("img")).toBeInTheDocument();
   expect(screen.getByRole("img")).toBeInTheDocument();
 });
